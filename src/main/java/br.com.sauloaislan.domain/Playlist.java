89ec84cf4f08ca59957f08293cb51bbe51360660
SauloAislan <sauloaislan20@gmail.com>
2019-09-26 16:22:18 -0300

    Second commit

---
+++ src/main/java/br.com.sauloaislan.domain/Playlist.java
@@ -0,0 +1,10 @@
+package src.main.java.br.com.sauloaislan.domain;
+
+/**
+ * @author SauloAislan
+ * created on 26/09/2019
+ * inside the package - src.main.java.br.com.sauloaislan.domain
+ **/
+
+public class Playlist {
+}
